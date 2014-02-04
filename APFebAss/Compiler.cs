bac9dd0a90ca7481b61897f5726c35fbe26c769a
JammyITA <giammi121@gmail.com>
2014-02-04 15:49:14 +0100

    Compiler rewritten. Now C90 compatible
    Compiler now works

--- APFebAss/Compiler.cs
+++ APFebAss/Compiler.cs
@@ -35,8 +35,6 @@ namespace APFebAss
 
             StringBuilder compiledCode = new StringBuilder();
 
-            compiledCode.AppendLine("//Warning: this code is incopatible with ISO C90 standard");
-            compiledCode.AppendLine("//tested with gcc 4.0.4");
             compiledCode.AppendLine("#include <stdio.h>");
             compiledCode.AppendLine("#include <stdlib.h>");
             compiledCode.AppendLine("typedef int bool;");
@@ -55,20 +53,8 @@ namespace APFebAss
 
             //Console.WriteLine("\n\n****************\n" + compiledCode + "\n****************\n\n");
 
-
-            //return compiledCode.ToString();
-            return deleteResultLines(compiledCode.ToString());
-
+            return compiledCode.ToString();
         }
-
-        //With this function I want to delete the unnecessary lines with only  a "result" variable, generated
-        //by a nested "and" with some "let" inside. Considered his cost, I decide to rely on the optimization
-        //performed by c's compiler and not use this function.
-        string deleteResultLines(String s)
-        {
-            return Regex.Replace(s, "result = result[ && result]*[ ]*;", "");
-        }
-
     }
 
 }
